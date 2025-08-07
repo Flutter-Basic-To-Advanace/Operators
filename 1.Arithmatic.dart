a0293304a767d22ad479cc7df69adc455a8504a1
sharada417 <sharada.kanchana2001@gmail.com>
2025-08-08 02:38:04 +0530

    Finish work

---
+++ 1.Arithmatic.dart
@@ -0,0 +1,24 @@
+void main() {
+  int a = 10;
+  int b = 3;
+
+  print("a = $a, b = $b");
+
+  // Addition
+  print("Addition: a + b = ${a + b}");
+
+  // Subtraction
+  print("Subtraction: a - b = ${a - b}");
+
+  // Multiplication
+  print("Multiplication: a * b = ${a * b}");
+
+  // Division
+  print("Division: a / b = ${a / b}");
+
+  // Integer Division
+  print("Integer Division: a ~/ b = ${a ~/ b}");
+
+  // Modulus
+  print("Modulus: a % b = ${a % b}");
+}
